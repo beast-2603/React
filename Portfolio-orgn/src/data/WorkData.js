9f2f618701fa529afb3581a57106bb2844484fff
beast-2603 <senapati.sohan31@gmail.com>
2022-11-15 11:54:34 +0530

    minor changes

--- Portfolio-orgn/src/data/WorkData.js
+++ Portfolio-orgn/src/data/WorkData.js
@@ -1,62 +1,62 @@
 export const Work = [
-    {
-        id:1,
-        name:"Portfolio-Template",
-        description:"It is made with React.Js and styled components, it includes simple and fun animations",
-        tags:["react","styledComponents","non-responsive"],
-        
-        demo:"https://portfolio-4b009.web.app/",
-        github:"https://github.com/beast-2603/Reacts/tree/main/portfolio-1"
-    },
-    {
-        id:2,
-        name:"Tesla-clone",
-        description:"A clone of the original Tesla Website and my first ever hosted website",
-        tags:["react","styledComponents","Figma","non-responsive"],
-        
-        demo:"https://clones-e3605.web.app/",
-        github:"https://github.com/beast-2603/Reacts/tree/main/tesla-clone"
-    }
-        
-    // ,
-    // {
-    //     id:3,
-    //     name:"Dev.to Clone",
-    //     description:"A clone of Dev.to build using ReactJS. With realtime fetching of articles from dev.to using API.",
-    //     tags:["react","api","sass"],
-        
-    //     demo:"http://devto-clone-codebucks27.vercel.app/",
-    //     github:"https://github.com/codebucks27/devto-clone"
-    // },
-    // {
-    //     id:4,
-    //     name:"Sidebar Menu",
-    //     description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
-    //     tags:["react","styledComponents","react-router"],
-        
-    //     demo:"https://react-sidebar.vercel.app/",
-    //     github:"https://github.com/codebucks27/react-sidebar"
-       
-    // },{
-    //     id:5,
-    //     name:"Todo App(React + Redux)",
-    //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
-    //     tags:["react","redux","framer-motion"],
-        
-    //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
-    //     github:"https://github.com/codebucks27/React-Redux-Todo-App"
-        
-    // },
-    // {
-    //     id:6,
-    //     name:"Responsive Navigation Menu",
-    //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
-    //     tags:["react","react-router","css"],
-        
-    //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
-    //     github:"https://github.com/codebucks27/React-responsive-navbar"
-        
-    // },
-    
-  
-] 
+  {
+    id: 1,
+    name: 'Portfolio-Template',
+    description:
+      'It is made with React.Js and styled components, it includes simple and fun animations',
+    tags: ['react', 'styledComponents', 'non-responsive'],
+
+    demo: 'https://portfolio-4b009.web.app/',
+    github: 'https://github.com/beast-2603/Reacts/tree/main/portfolio-1'
+  },
+  {
+    id: 2,
+    name: 'Tesla-clone',
+    description:
+      'A clone of the original Tesla Website and my first ever hosted website',
+    tags: ['react', 'styledComponents', 'Figma', 'non-responsive'],
+
+    demo: 'https://clones-e3605.web.app/',
+    github: 'https://github.com/beast-2603/Reacts/tree/main/tesla-clone'
+  },
+  {
+    id: 3,
+    name: 'Box-Office App',
+    description:
+      'A interactive webpage for people interested in movies. Search for your favourite movies and actors',
+    tags: ['react', 'maze-api', 'sass', 'react-hooks'],
+
+    demo: 'https://beast-2603.github.io/box-office/#/',
+    github: 'https://github.com/beast-2603/box-office'
+  },
+  {
+    id: 4,
+    name: 'Tic-Tac_Toe',
+    description:
+      'A cool and interesting dula player game of X and O, come visit if you are bored',
+    tags: ['react', 'styledComponents', 'react-router'],
+
+    demo: 'https://tictactoebeast.surge.sh/',
+    github: 'https://github.com/beast-2603/tictactoe'
+  }
+  // {
+  //     id:5,
+  //     name:"Todo App(React + Redux)",
+  //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+  //     tags:["react","redux","framer-motion"],
+
+  //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+  //     github:"https://github.com/codebucks27/React-Redux-Todo-App"
+
+  // },
+  // {
+  //     id:6,
+  //     name:"Responsive Navigation Menu",
+  //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+  //     tags:["react","react-router","css"],
+
+  //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+  //     github:"https://github.com/codebucks27/React-responsive-navbar"
+
+  // },
+];
--- Portfolio-orgn/src/subComponents/SoundBar.js
+++ Portfolio-orgn/src/subComponents/SoundBar.js
@@ -1,34 +1,32 @@
-import { motion } from 'framer-motion'
-import React, { useRef, useState } from 'react'
-import styled, { keyframes } from 'styled-components'
-import music from '../assets/audio/u-said-it-v13-1167.mp3'
-
+import { motion } from 'framer-motion';
+import React, { useRef, useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+import music from '../assets/audio/Electro-Light-symbolism-mp3-song.mp3';
 
 const Box = styled.div`
-display: flex;
-cursor: pointer;
-position: fixed;
-left: 8rem;
-top: 3rem;
-z-index: 10;
+  display: flex;
+  cursor: pointer;
+  position: fixed;
+  left: 8rem;
+  top: 3rem;
+  z-index: 10;
 
-&>*:nth-child(1){
+  & > *:nth-child(1) {
     animation-delay: 0.2s;
-}
-&>*:nth-child(2){
+  }
+  & > *:nth-child(2) {
     animation-delay: 0.3s;
-}
-&>*:nth-child(3){
+  }
+  & > *:nth-child(3) {
     animation-delay: 0.4s;
-}
-&>*:nth-child(4){
+  }
+  & > *:nth-child(4) {
     animation-delay: 0.5s;
-}
-&>*:nth-child(5){
+  }
+  & > *:nth-child(5) {
     animation-delay: 0.8s;
-}
-`
-
+  }
+`;
 
 const play = keyframes`
 0%{
@@ -40,92 +38,94 @@ const play = keyframes`
 100%{
     transform:scaleY(.75)
 }
-`
-
+`;
 
 const Line = styled(motion.span)`
-background: ${props => props.theme.text};
-border: 1px solid ${props => props.theme.body};
+  background: ${(props) => props.theme.text};
+  border: 1px solid ${(props) => props.theme.body};
 
-animation: ${play} 1s ease infinite;
-animation-play-state: ${props => props.click ? "running" : "paused"};
-height: 1rem;
-width: 2px;
-margin: 0 0.1rem;
-`
-
-
+  animation: ${play} 1s ease infinite;
+  animation-play-state: ${(props) => (props.click ? 'running' : 'paused')};
+  height: 1rem;
+  width: 2px;
+  margin: 0 0.1rem;
+`;
 
 const SoundBar = () => {
-    const ref = useRef(null);
-    const [click, setClick] = useState(false);
+  const ref = useRef(null);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => {
-        setClick(!click);
-        
-        if(!click){
-            ref.current.play();
-        }else{
-            ref.current.pause();
-        }
+  const handleClick = () => {
+    setClick(!click);
+
+    if (!click) {
+      ref.current.play();
+    } else {
+      ref.current.pause();
     }
+  };
 
   return (
     <Box onClick={() => handleClick()}>
-        <Line click={click} 
-        initial = {{
-            opacity: 0,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          animate = {{
-            opacity: 1,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          />
-        <Line click={click} 
-        initial = {{
-            opacity: 0,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          animate = {{
-            opacity: 1,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          />
-        <Line click={click} 
-        initial = {{
-            opacity: 0,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          animate = {{
-            opacity: 1,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          />
-        <Line click={click} 
-        initial = {{
-            opacity: 0,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          animate = {{
-            opacity: 1,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          />
-        <Line click={click} 
-        initial = {{
-            opacity: 0,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          animate = {{
-            opacity: 1,
-            transition: { type:'spring', duration: 1.5, delay:1 }
-          }}
-          />
+      <Line
+        click={click}
+        initial={{
+          opacity: 0,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+        animate={{
+          opacity: 1,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+      />
+      <Line
+        click={click}
+        initial={{
+          opacity: 0,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+        animate={{
+          opacity: 1,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+      />
+      <Line
+        click={click}
+        initial={{
+          opacity: 0,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+        animate={{
+          opacity: 1,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+      />
+      <Line
+        click={click}
+        initial={{
+          opacity: 0,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+        animate={{
+          opacity: 1,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+      />
+      <Line
+        click={click}
+        initial={{
+          opacity: 0,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+        animate={{
+          opacity: 1,
+          transition: { type: 'spring', duration: 1.5, delay: 1 }
+        }}
+      />
 
-        <audio src={music} ref={ref} loop />
+      <audio src={music} ref={ref} loop />
     </Box>
-  )
-}
+  );
+};
 
-export default SoundBar
+export default SoundBar;
